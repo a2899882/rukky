c60563ae37f099b06be691a0703fd71450fc96c2
a2899882 <morckylee@gmail.com>
2025-12-16 04:58:05 +0800

    chore: emit gunicorn logs to stdout for debugging

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -20,4 +20,7 @@ fi
 exec gunicorn server.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers "${GUNICORN_WORKERS:-2}" \
-  --timeout 120
+  --timeout 120 \
+  --access-logfile - \
+  --error-logfile - \
+  --log-level info
